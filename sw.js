857d98e0e86259ad7ed629f3c7d93751b33b1913
OmarSaadeldin <omarsaadeldin5909@gmail.com>
2026-06-20 13:39:14 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'seerah-v4';
+
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(k => caches.delete(k))
+    ))
+  );
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(res => {
+      let clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
